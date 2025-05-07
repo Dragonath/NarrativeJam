7f3d7c637b90d5003995d674b909687e54139802
Aku Telimaa <akuii.wabbit@gmail.com>
2025-05-07 19:38:07 +0300

    Tileset + Room Grids
    Main Camera modified with new script to change room.
    Grid and Room grid prefab for blank levels.

---
+++ Assets/Scripts/CameraRoomSwap.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class CameraRoomSwap : MonoBehaviour
+{
+    public Grid grid;
+    public GameObject player;
+    public GameObject mainCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3Int cellPos = grid.WorldToCell(player.transform.position);
+        mainCamera.transform.position = grid.GetCellCenterWorld(cellPos);
+    }
+}
--- Assets/Scripts/ControllerPlayer.cs
+++ Assets/Scripts/ControllerPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// OLD ONE - REMOVE LATER 
 
 public class PlayerControls : MonoBehaviour
 {
